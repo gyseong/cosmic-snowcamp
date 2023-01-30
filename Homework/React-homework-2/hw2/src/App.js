23d847282cb4c9163b9bed3b6469b230f50a7bd7
gyseong <95284588+gyseong@users.noreply.github.com>
2023-01-30 20:00:03 +0900

    Update App.js

--- Homework/React-homework-2/hw2/src/App.js
+++ Homework/React-homework-2/hw2/src/App.js
@@ -5,9 +5,10 @@ import axios from 'axios';
 import API from './api';
 import Table from 'react-bootstrap/Table';
 
+
 function App() {
 
-  const [data, setData]= useState({hits:[]});
+  const [data, setData]= useState({row:[]});
   const [query, setQuery] = useState("");
   const [search, setSearch] = useState("");
   const [loading, setLoading] = useState(false);
@@ -15,12 +16,15 @@ function App() {
 
   useEffect(() => {
     const fetchData = async () => {
-      const result = await axios(
-        `http://openapi.foodsafetykorea.go.kr/api/22c3f98a494f4e06977a/I2790/json/1/5/DESC_KOR=${search}`
+      if(search.length>0){
+      setLoading(true);
+      let result = await axios(
+        `http://cors-anywhere.herokuapp.com/http://openapi.foodsafetykorea.go.kr/api/22c3f98a494f4e06977a/I2790/json/1/5/DESC_KOR=${search}`
       );
       setData(result.data);
       console.log(result.data);
-      console.log(search.length);
+      setLoading(false);
+      }
     };
     fetchData();
   }, [search]);
@@ -34,21 +38,18 @@ function App() {
         value={query}
         onChange={(e) => setQuery(e.target.value)}
       />
-     <button type="button" onClick={() => setSearch(query)}>
-       검색
-      </button>
-      <Table striped>
-      <tbody>
-        {data.length>0 && data.hits.map((item) => (
-        <tr key={item.NUM}>
-          <td>{item.DESC}</td>
-          <td>{item.GROUP_NAME}</td>
-          <td>{item.MAKER_NAME}</td>
-        </tr>
-      ))}
-
-        </tbody>
-      </Table>
+     <button type="button" onClick={() => setSearch(query)}>검색</button>
+     {loading ? (
+        <div>불러오는 중입니다...</div>
+      ) : (
+        <ul>
+          {data.length>0 && data.row.map((item) => (
+            <li key={item.NUM}>
+              <div>{item.DESC_KOR}</div>
+            </li>
+          ))}
+          </ul>
+      )}
    </>
   );
 }
