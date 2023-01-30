ceae6a0ef616660374418428c8ff1ab045cbb620
gyseong <95284588+gyseong@users.noreply.github.com>
2023-01-30 19:57:26 +0900

    Update food.jsx

--- Homework/React-homework-2/hw2/src/food.jsx
+++ Homework/React-homework-2/hw2/src/food.jsx
@@ -1,13 +1,38 @@
-import React from 'react';
-import FoodItem from './food_item';
+import Table 
+from 'react-bootstrap/esm/Table';
+import {useState,useEffect, Fragment} from 'react';
+import axios from 'axios';
 
-const FoodList = (props) => {
+export function FoodData(props) {
+    const [data, setData]= useState({hits:[]});
+    const [query, setQuery] = useState("");
+    const [search, setSearch] = useState("");
+    const [loading, setLoading] = useState(false);
+  
+    useEffect(() => {
+      const fetchData = async () => {
+        const result = await axios(
+          `http://cors-anywhere.herokuapp.com/http://openapi.foodsafetykorea.go.kr/api/22c3f98a494f4e06977a/I2790/json/1/5/DESC_KOR=${props.search}`
+        );
+        setData(result.data);
+        console.log(result.data);
+      };
+      fetchData();
+    }, [search]);
+
     return (
-        <div>
-            {props.foodname.map(inputValue => (
-                <FoodItem key={inputValue.DESC_KOR}  inputvalue={inputValue} />
-            ))}
-        </div>
-    );
-};
-export default FoodList;
+        <Table striped>
+      <tbody>
+        {data.length>0 && data.hits.map((item) => (
+        <tr key={item.NUM}>
+          <td>{item.DESC}</td>
+          <td>{item.GROUP_NAME}</td>
+          <td>{item.MAKER_NAME}</td>
+        </tr>
+      ))}
+
+        </tbody>
+      </Table>
+    )
+}
+export default FoodData;
